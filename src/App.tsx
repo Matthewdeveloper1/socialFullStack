c9599b6e57e817b006f580526b3b9bb7f16d08fb
Matthewdeveloper1 <matthew.gelich@gmail.com>
2025-02-13 21:23:33 +0300

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css"
 
 const App = () => {
+
   return (
     <div className="App">
-      a
+      <button className="border border-1 border-blue-500">a</button>
     </div>
   )
 }
